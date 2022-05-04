9e076a409035096297f81b85c8fd25b2b9e66d0f
Samik Malhotra <samikmalhotra66@gmail.com>
2022-05-04 14:52:08 +0530

    singin and signup client functionality

---
+++ client/components/authForm.tsx
@@ -0,0 +1,82 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { FC, Fragment, useState } from "react";
+import { useSWRConfig } from "swr";
+import NextImage from "next/image";
+import { auth } from "../lib/mutations";
+
+const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await auth(mode, { email, password, firstName, lastName });
+    setIsLoading(false);
+    router.push("/");
+  };
+
+  return (
+    <Box height="100vh" width="100vw" bg="black" color="white">
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            {mode === "signup" && (
+              <Fragment>
+                <Input
+                  placeholder="First Name"
+                  type="text"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Input
+                  placeholder="Last Name"
+                  type="text"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Fragment>
+            )}
+
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.300",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default AuthForm;
