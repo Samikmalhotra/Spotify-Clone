8637f9f299e986b388414dcfa0fdc399139ad8de
Samik Malhotra <samikmalhotra66@gmail.com>
2022-05-14 08:40:06 +0530

    added gradient layout

--- client/components/playerLayout.tsx
+++ client/components/playerLayout.tsx
@@ -1,19 +1,18 @@
 import { Box } from "@chakra-ui/layout";
 import Sidebar from "./sidebar";
 
-
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
-        <Box position="absolute" top="0" width="250px" left="0">
-            <Sidebar/>
-        </Box>
-        <Box marginLeft="250px">
-            {children}
-        </Box>
-        <Box position="absolute" left="0" bottom="0">
-            Layer
-        </Box>
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px">
+        <Box height="calc(100vh - 100px)">{children}</Box>
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        Layer
+      </Box>
     </Box>
   );
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
+import prisma from '../lib/prisma'
 
-export default function Home() {
+export default function Home({artists}) {
   return (
-    <div>Home</div>
+    <GradientLayout color={"red"}>
+      <div>home page</div>
+    </GradientLayout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: {artists},
+  }
 }
