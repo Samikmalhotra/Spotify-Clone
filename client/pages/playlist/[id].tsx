700cc7e0bddee17e23cbf720da5f17cd2d623866
Samik Malhotra <samikmalhotra66@gmail.com>
2022-05-14 11:08:37 +0530

    added song table

--- client/pages/playlist/[id].tsx
+++ client/pages/playlist/[id].tsx
@@ -1,59 +1,57 @@
-import GradientLayout from '../../components/gradientLayout'
-import { validateToken } from '../../lib/auth'
+import GradientLayout from "../../components/gradientLayout";
+import SongTable from "../../components/songTable";
+import { validateToken } from "../../lib/auth";
 // import SongTable from '../../components/songsTable'
 // import { validateToken } from '../../lib/auth'
-import prisma from '../../lib/prisma'
+import prisma from "../../lib/prisma";
 
 const getBGColor = (id) => {
   const colors = [
-    'red',
-    'green',
-    'blue',
-    'orange',
-    'purple',
-    'gray',
-    'teal',
-    'yellow',
-  ]
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
 
-  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
-}
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
 
 const Playlist = ({ playlist }) => {
+  const color = playlist ? getBGColor(playlist.id) : "gray";
 
-  const color = playlist ? getBGColor(playlist.id) : 'gray'
-
-    console.log(playlist)
+  console.log(playlist);
 
   return (
     <GradientLayout
-      color={color || 'black'}
+      color={color || "black"}
       roundImage={false}
-      title={playlist ? playlist.name: 'Playlist'}
+      title={playlist ? playlist.name : "Playlist"}
       subtitle="playlist"
       description={`${playlist && playlist.songs.length} songs`}
       image={`https://picsum.photos/400?random=${playlist && playlist.id}`}
     >
-      {/* <SongTable songs={playlist.songs} /> */}
+      <SongTable songs={playlist.songs} />
     </GradientLayout>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async ({ query, req }) => {
-  let user
+  let user;
 
   try {
-    user = validateToken(req.cookies.CHORDS_ACCESS_TOKEN)
+    user = validateToken(req.cookies.CHORDS_ACCESS_TOKEN);
   } catch (e) {
     return {
       redirect: {
         permanent: false,
-        destination: '/signin',
+        destination: "/signin",
       },
-    }
+    };
   }
-
-
 
   const [playlist] = await prisma.playlist.findMany({
     where: {
@@ -72,10 +70,10 @@ export const getServerSideProps = async ({ query, req }) => {
         },
       },
     },
-  })
+  });
 
   return {
-    props: {playlist},
-  }
-}
-export default Playlist
+    props: { playlist },
+  };
+};
+export default Playlist;
